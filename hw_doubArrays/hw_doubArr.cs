be42d17c6a4c2e828471e2b1850b94b227efd261
BekAs <bekas.omsk@mail.ru>
2023-12-21 21:11:53 +0600

    task1

--- hw_doubArrays/hw_doubArr.cs
+++ hw_doubArrays/hw_doubArr.cs
@@ -1,6 +1,4 @@
 ﻿/*
-Сдавать ссылкой на GITHUB(отправить в комментарии к дз)
-
 Задача 1: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
 и возвращает значение этого элемента или же указание, что такого элемента нет.
 
@@ -14,3 +12,33 @@
 Под удалением понимается создание нового двумерного массива без строки и столбца
 */
 Console.Clear();
+// int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+// Console.WriteLine("[" + string.Join(" ", array) + "]");
+// int[,] mtx = new int[array[0], array[1]];
+// int returnElem(int[,] mtx)
+// {
+//     int f = 0;
+//     return f;
+// }
+// int d = returnElem(mtx);
+Console.Write("Ввести количество строк: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ввести количество столбцов: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[,] mtx = new int[x, y];
+
+void createArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            mtx[i, j] = new Random().Next(0, 10);
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+createArray(mtx);
